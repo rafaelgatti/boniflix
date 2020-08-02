6cff85e136fac8ea0089f51d960390b2d4bb3b31
Rafael Gatti <rafaelgatti@MacBook-Pro-de-Rafael.local>
2020-08-02 01:05:51 -0300

    Setup inicial do Projeto

---
+++ src/components/Menu/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Logo from '../../assets/img/Logo.png'
+import './Menu.css'
+import Button from '../Button';
+//import ButtonLink from './components/ButtonLink';
+
+function Menu (){
+
+    return (
+        <nav className="Menu">
+            <a href="/">
+                <img src={Logo} className="Logo" alt="AluraFlix" />
+            </a>
+
+            <Button as="a" className="ButtonLink" href="/">
+                Novo vídeo
+            </Button>
+        </nav>
+    );
+}
+
+export default Menu;
