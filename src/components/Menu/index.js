4506a012801ff5ed3d6f539b9a1825783defe520
Rafael Gatti <rafaelgatti@MacBook-Pro-de-Rafael.local>
2020-08-03 23:42:24 -0300

    Commit aula 2 - Cadastros

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/Logo.png'
 import './Menu.css'
 import Button from '../Button';
@@ -8,11 +9,11 @@ function Menu (){
 
     return (
         <nav className="Menu">
-            <a href="/">
+            <Link to="/">
                 <img src={Logo} className="Logo" alt="AluraFlix" />
-            </a>
+            </Link>
 
-            <Button as="a" className="ButtonLink" href="/">
+            <Button as={Link} className="ButtonLink" to="/cadastro/video">
                 Novo vídeo
             </Button>
         </nav>
