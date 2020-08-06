3d211bfa8ff28663048df6699267200210e55fa7
Rafael Gatti <rafaelgatti@MacBook-Pro-de-Rafael.local>
2020-08-06 09:49:26 -0300

    Commit final aula 4

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -30,7 +30,7 @@ function CadastroCategoria() {
 
   useEffect(() => {
     if (window.location.href.includes('localhost')) {
-      const URL = 'http://localhost:8080/categorias';
+      const URL = 'https://boniflix.herokuapp.com/categorias';
       fetch(URL)
         .then(async (respostaDoServer) => {
           if (respostaDoServer.ok) {
@@ -90,10 +90,17 @@ function CadastroCategoria() {
           Cadastrar
         </Button>
       </form>
+
+      {categorias.length === 0 && (
+        <div>
+          Loading
+        </div>
+      )}
+
       <ul>
         {categorias.map((categoria) => (
           <li key={`${categoria.id}`}>
-            {categoria.titulo}
+            {categoria.nome}
           </li>
         ))}
       </ul>
